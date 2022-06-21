16a3cab4f2ecfdf849fd547c03312cbb59e91777
Vishal Kumar Thakur <vishalinfobeans>
2022-06-21 19:14:42 +0530

    first commit

---
+++ src/main/java/com/firstProject/firstProject/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.firstProject.firstProject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@GetMapping("/test")
+	public String getDemoName() {
+		return "Hello WOrld !";
+	}
+}
